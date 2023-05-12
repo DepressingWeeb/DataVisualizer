b8032bc707ec00f61d8f341b21ddf01ec02a63ac
Buu Pham <pphambuu@gmail.com>
2023-05-12 14:45:24 +0700

    edit main function

--- DataVisualizer.cpp
+++ DataVisualizer.cpp
@@ -1,25 +1,9 @@
-#include "imgui.h"
-#include "imgui_impl_sdl2.h"
-#include "imgui_impl_sdlrenderer.h"
-#include <stdio.h>
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
-#include <string>
-#include <time.h>
-#include <vector>
-#include <utility>
-#include <sstream>
-#include <fstream>
-#include <tuple>
-#include <map>
-#include "global.h"
 #include "utils.h"
 #include "UI_components.h"
 int main(int, char**)
 {
 	init();
-	numberFont= TTF_OpenFont("resources/Font/SpaceMono-Regular.ttf", 300);
+	initResource();
 	startingScreen();
 	return 0;
 }
